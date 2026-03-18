b027d971bfe8100f7365eef7ba09f16d068e4114
minotto <minotto115@gmail.com>
2026-03-18 19:54:45 +0900

    faviconを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import "./layout.css";
+	import lightFavicon from "$lib/assets/favicon-light.svg";
+	import darkFavicon from "$lib/assets/favicon-dark.svg";
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={lightFavicon} media="(prefers-color-scheme: light)" />
+	<link rel="icon" href={darkFavicon} media="(prefers-color-scheme: dark)" />
+</svelte:head>
 {@render children()}
